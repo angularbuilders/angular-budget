1762c0534832874bf93e1e3ab008464ee505311e
Alberto Basalo <albertobasalo@hotmail.com>
2020-03-16 17:59:15 +0100

    ⚙: minor

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 
-
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+  imports: [CommonModule, ProjectsRoutingModule],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
