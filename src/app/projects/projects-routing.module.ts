f452d046d237b043febaa8757904a694b90f4c06
Alberto Basalo <albertobasalo@hotmail.com>
2020-10-28 12:36:13 +0100

    feat: pagina de proyecto #17

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
 import { ProjectsComponent } from './projects.component';
 
-const routes: Routes = [{ path: '', component: ProjectsComponent }, { path: 'new', loadChildren: () => import('./new-project/new-project.module').then(m => m.NewProjectModule) }];
+
+const routes: Routes = [
+  { path: '', component: ProjectsComponent },
+  { path: 'new', loadChildren: () => import('./new-project/new-project.module').then(m => m.NewProjectModule) },
+  { path: ':id', component: ProjectComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
