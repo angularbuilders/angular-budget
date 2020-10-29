36f913392407c1ab6dc8433d2b3eecb42d2f461d
Alberto Basalo <albertobasalo@hotmail.com>
2020-10-29 14:34:52 +0100

    feat: fromualrio para alta de proyectos - close#23

--- src/app/projects/new-project/new-project.component.ts
+++ src/app/projects/new-project/new-project.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/core/model/project.interface';
+import { Status } from 'src/app/core/model/status.enum';
+import { PROJECTS } from 'src/data/projects.data';
 
 @Component({
   selector: 'ab-new-project',
   templateUrl: './new-project.component.html',
-  styleUrls: ['./new-project.component.css']
+  styleUrls: ['./new-project.component.css'],
 })
 export class NewProjectComponent implements OnInit {
-
-  constructor() { }
+  newProject: Project;
+  constructor() {}
 
   ngOnInit(): void {
+    this.resetNewProjet();
   }
 
+  resetNewProjet(): void {
+    this.newProject = {
+      id: '',
+      title: '',
+      start: new Date(),
+      budget: 0,
+      description: '',
+      status: Status.InProgress,
+    };
+  }
+
+  saveNewProject(): void {
+    this.newProject.id = this.slugify(this.newProject.title);
+    PROJECTS.push({ ...this.newProject });
+    this.resetNewProjet();
+  }
+  slugify(text: string): string {
+    return text
+      .toLowerCase()
+      .trim()
+      .replace(/[\s\W-]+/g, '-');
+  }
 }
--- src/app/projects/new-project/new-project.module.ts
+++ src/app/projects/new-project/new-project.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NewProjectRoutingModule } from './new-project-routing.module';
 import { NewProjectComponent } from './new-project.component';
 
-
 @NgModule({
   declarations: [NewProjectComponent],
-  imports: [
-    CommonModule,
-    NewProjectRoutingModule
-  ]
+  imports: [CommonModule, NewProjectRoutingModule, FormsModule],
 })
-export class NewProjectModule { }
+export class NewProjectModule {}
