a0c06f2495bc81e0e2233407dd38873a69b5d870
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-02 14:04:24 +0100

    Auto-generated commit

--- common.0380c8989f1efebff7a7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"2FCU":function(n,o,e){"use strict";e.d(o,"a",(function(){return s}));var s=function(n){return n.Incoming="Ingreso",n.Expense="Gasto",n}({})}}]);
