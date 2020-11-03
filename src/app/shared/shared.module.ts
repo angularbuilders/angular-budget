b1998933fc50a98e725e0736e3b7c713c084ab59
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-03 14:10:37 +0100

    fix: simplificacion articulo nodata - close#37

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ArticleComponent } from './article/article.component';
 import { NoDataYetComponent } from './no-data-yet/no-data-yet.component';
-import { DataWaiterComponent } from './data-waiter/data-waiter.component';
-
-
 
 @NgModule({
-  declarations: [ArticleComponent, NoDataYetComponent, DataWaiterComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [ArticleComponent, NoDataYetComponent, DataWaiterComponent]
+  declarations: [ArticleComponent, NoDataYetComponent],
+  imports: [CommonModule],
+  exports: [ArticleComponent, NoDataYetComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
