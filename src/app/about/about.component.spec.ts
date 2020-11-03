282766ea1d1f91a3f337af6625ee5984a56cddd2
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-03 18:14:53 +0100

    test: mínimo de un componente - close #38

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,25 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutComponent } from './about.component';
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+fdescribe('GIVEN: the AboutComponent', () => {
+  beforeEach(() => {});
+  it('WHEN Ask for title THEN equals Angular Budget', () => {
+    const sut = new AboutComponent();
+    const actual = sut.title;
+    const expected = 'Angular Budget';
+    expect(actual).toEqual(expected);
   });
 });
+
+/**
+ * Original Angular CLI generated Code
+ */
+
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// describe('AboutComponent', () => {
+//   let component: AboutComponent;
+//   let fixture: ComponentFixture<AboutComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ AboutComponent ]
+//     })
+//     .compileComponents();
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AboutComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ab-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  public title = 'Angular Budget';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
