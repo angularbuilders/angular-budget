0f61e511271b41a8f700686075b81f8d40c2bb7f
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-03 13:19:50 +0100

    wip: ng g c shared/no-data-yet --export #37

---
+++ src/app/shared/no-data-yet/no-data-yet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ab-no-data-yet',
+  templateUrl: './no-data-yet.component.html',
+  styleUrls: ['./no-data-yet.component.css']
+})
+export class NoDataYetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article/article.component';
+import { NoDataYetComponent } from './no-data-yet/no-data-yet.component';
 
 
 
 @NgModule({
-  declarations: [ArticleComponent],
+  declarations: [ArticleComponent, NoDataYetComponent],
   imports: [
     CommonModule
   ],
-  exports: [ArticleComponent]
+  exports: [ArticleComponent, NoDataYetComponent]
 })
 export class SharedModule { }
