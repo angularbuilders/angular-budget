5429ac667337a8c6d6259e1a4d5e5ce54e8e3d6d
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-04 12:41:08 +0100

    wip:  ng g c shared/dates --export #41

---
+++ src/app/shared/dates/dates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ab-dates',
+  templateUrl: './dates.component.html',
+  styleUrls: ['./dates.component.css']
+})
+export class DatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ArticleComponent } from './article/article.component';
 import { NoDataYetComponent } from './no-data-yet/no-data-yet.component';
+import { DatesComponent } from './dates/dates.component';
 
 @NgModule({
-  declarations: [ArticleComponent, NoDataYetComponent],
+  declarations: [ArticleComponent, NoDataYetComponent, DatesComponent],
   imports: [CommonModule],
-  exports: [ArticleComponent, NoDataYetComponent],
+  exports: [ArticleComponent, NoDataYetComponent, DatesComponent],
 })
 export class SharedModule {}
