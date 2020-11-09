7e07aa0700fc534e34044fda8833b3f5018bde14
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-09 13:59:46 +0100

    test: inyección de dependencia HttpClient - close#53

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,13 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataService } from 'src/app/core/services/data.service';
+import { LogicService } from 'src/app/core/services/logic.service';
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
+      imports: [HttpClientTestingModule],
+      providers: [DataService, LogicService],
     }).compileComponents();
   });
 
