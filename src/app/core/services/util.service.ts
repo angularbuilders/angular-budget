56d6ba232a3d89f63b20b2793e20b7ff5f1b4fde
Alberto Basalo <albertobasalo@hotmail.com>
2020-11-09 11:10:44 +0100

    feat: pipe timeAgo para fecha coloquial close#48

--- src/app/core/services/util.service.ts
+++ src/app/core/services/util.service.ts
@@ -7,6 +7,11 @@ import { ActivatedRoute, Event, NavigationEnd } from '@angular/router';
 })
 export class UtilService {
   private siteTitle = 'Angular.Budget';
+  private coloquiales = [
+    { hasta: 1000 * 60 * 60 * 24 * 1, expresion: 'un día' },
+    { hasta: 1000 * 60 * 60 * 24 * 7, expresion: 'una semana' },
+    { hasta: 1000 * 60 * 60 * 24 * 30, expresion: 'un mes' },
+  ];
 
   constructor(private titleService: Title) {}
 
@@ -31,5 +36,16 @@ export class UtilService {
     } else {
       return activatedRoute;
     }
+  }
+  public getFechaColoquial(fecha: Date): string {
+    const ahora = Date.now();
+    const momento = new Date(fecha).getTime();
+    const diferencia = momento - ahora;
+    const antesDespues = diferencia > 0 ? 'dentro de ' : 'hace ';
+    const absDiferencia = Math.abs(diferencia);
+    const coloquial = this.coloquiales.find(c => c.hasta > absDiferencia);
+    const expresionDefault = 'mucho tiempo';
+    const expresion = coloquial != undefined ? coloquial.expresion : expresionDefault;
+    return antesDespues + expresion;
   }
 }
