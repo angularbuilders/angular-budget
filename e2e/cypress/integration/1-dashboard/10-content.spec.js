537f1f642f885443594a07169680fa86a12e1f06
Alberto Basalo <albertobasalo@hotmail.com>
2020-12-01 13:26:40 +0100

    test: pruebas de contenido y navegación close#70

---
+++ e2e/cypress/integration/1-dashboard/10-content.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="Cypress" />
+
+// FEATURE:     General status of my projects
+// As a:        user with various projects
+// I want to:   view a list of projects and a summary of taskss
+// In order to: plan my work
+
+// SUT: https://angularbuilders.github.io/angular-budget/
+
+// Scenario: View a the dashboard
+//  GIVEN: A web app where i have 2 projects
+//  WHEN: I visit the home page
+//  THEN: should have a section called Mis Proyectos
+//  THEN: should show 2 items on the projects list
+
+describe(`GIVEN: A web app where i have 2 projects`, () => {
+  // Arrange
+  const sutUrl = 'https://angularbuilders.github.io/angular-budget/';
+  context(`WHEN: I visit the home page`, () => {
+    // Act
+    before(() => cy.visit(sutUrl));
+    it(`THEN: should have a section called Mis Proyectos`, () => {
+      const expected = 'Mis Proyectos';
+      // Assert
+      const actual = cy.contains(expected);
+      actual.should('exist');
+    });
+    it(`THEN: should show 2 items on the projects list`, () => {
+      // Assert
+      const expected = 2;
+      cy.get('aside:nth-child(1) dt').should('have.length', expected);
+    });
+  });
+});
---
+++ e2e/cypress/integration/1-dashboard/11-navigate.spec.js
@@ -0,0 +1,34 @@
+// FEATURE:     View detailed projects info
+// As a:        user with various projects
+// I want to:   view a table with deatiled project records
+// In order to: plan my work
+
+// SUT: https://angularbuilders.github.io/angular-budget/
+
+// Scenario: Navigate form the dashboard
+//  GIVEN: A web app with a menu
+//  WHEN: I click on proyectos menu
+//  THEN: should navigate to the proyectos page
+//  THEN: should see a table with 2 records
+
+describe(`GIVEN: A web app with a menu`, () => {
+  // Arrange
+  const sutUrl = 'https://angularbuilders.github.io/angular-budget/';
+  context(`WHEN: I click on proyectos menu`, () => {
+    // Act
+    before(() => {
+      cy.visit(sutUrl);
+      cy.get('a').contains('Proyectos').click();
+    });
+    it(`THEN: should navigate to the proyectos page`, () => {
+      // Assert
+      const expected = 'Proyectos | Angular.Budget';
+      cy.title().should('include', expected);
+    });
+    it(`THEN: should see a table with 2 records`, () => {
+      // Assert
+      const expected = 1 + 2;
+      cy.get('table tr').should('have.length', expected);
+    });
+  });
+});
